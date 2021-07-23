34b0e5c25dd91d4cf6fa1da117b9e9e2c7c3f33b
lukes1582 <lukes1582@gmail.com>
2021-07-23 14:57:30 +0200

    php-scripts

---
+++ ex_002.php
@@ -0,0 +1,23 @@
+<?php
+    echo("<!DOCTYPE html>");
+    echo( "    <html>");
+    echo(  "     <body>");
+
+
+
+    echo( "<h1>PHP is Fun!</h1>");
+    echo( "<h2>PHP is Fun!</h2>");
+    echo( "<h3>PHP is Fun!</h3>");
+    echo( "<h4> PHP </h4>");
+    
+    echo( "Hello world!<br>");
+    echo( "I'm about to learn PHP!<br>");
+    echo( "This ", "string ", "was ", "made ", "with multiple parameters.");
+    
+    echo(  "</body>");
+    echo(  "</html>");
+
+
+
+
+?>
